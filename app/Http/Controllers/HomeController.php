f0f83cf72db61f527ec6df050400b08f09af1925
wandoy37 <riswandi035@gmail.com>
2023-08-22 21:21:26 +0800

    next add comit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Sertifikat;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -12,10 +13,54 @@ class HomeController extends Controller
         return view('home.index');
     }
 
+    public function find(Request $request)
+    {
+        return redirect()->route('home.show', $request->id);
+    }
+
     public function show($id)
     {
-        $sertifikat = Sertifikat::find($id);
+        $sertifikat = DB::table('sertifikats')
+            ->join('kegiatans', 'sertifikats.kegiatan_id', '=', 'kegiatans.id')
+            ->join('pesertas', 'sertifikats.peserta_id', '=', 'pesertas.id')
+            ->join('kategoris', 'kegiatans.kategori_id', '=', 'kategoris.id')
+            ->join('penandatangans', 'kegiatans.penandatangan_id', '=', 'penandatangans.id')
+            ->where('sertifikats.id', $id)
+            ->select(
+                'sertifikats.id',
+                'sertifikats.status',
+                'kegiatans.id AS id_kegiatan',
+                'kegiatans.kode_kegiatan AS kode_kegiatan',
+                'kegiatans.judul_kegiatan AS judul_kegiatan',
+                'kategoris.title AS kategori_kegiatan',
+                'kategoris.template AS template_sertifikat',
+                'kegiatans.tahun_kegiatan AS tahun_kegiatan',
+                'kegiatans.tanggal_mulai_kegiatan AS tanggal_mulai_kegiatan',
+                'kegiatans.tanggal_akhir_kegiatan AS tanggal_akhir_kegiatan',
+                'kegiatans.total_jam_kegiatan AS total_jam_kegiatan',
+                'pesertas.nama AS nama_peserta',
+                'pesertas.nomor_identitas AS nomor_identitas_peserta',
+                'pesertas.tempat_lahir AS tempat_lahir_peserta',
+                'pesertas.tanggal_lahir AS tanggal_lahir_peserta',
+                'pesertas.jenis_kelamin AS jenis_kelamin_peserta',
+                'pesertas.foto AS foto_peserta',
+                'pesertas.pangkat_golongan AS pangkat_golongan_peserta',
+                'pesertas.jabatan AS jabatan_peserta',
+                'pesertas.instansi AS instansi_peserta',
+                'penandatangans.nama AS nama_penandatangan',
+                'penandatangans.nip AS nip_penandatangan',
+                'penandatangans.pangkat_golongan AS pangkat_golongan_penandatangan',
+                'penandatangans.jabatan AS jabatan_penandatangan',
+                'penandatangans.tanda_tangan_stempel AS tanda_tangan_stempel',
+            )
+            ->first();
+
+        if (!$sertifikat) {
+            $pesan = 'Sertifikat dengan kode ' . $id . ' tidak ditemukan';
+            return view('home.show_error', compact('pesan'));
+        }
         $pathToFile = public_path('sertifikat/' . 'doc-sertifikat-' . $sertifikat->id . '.' . 'pdf');
-        return response()->file($pathToFile);
+        // return response()->file($pathToFile);
+        return view('home.show', compact('sertifikat'));
     }
 }
---
+++ resources/views/home/show.blade.php
@@ -0,0 +1,46 @@
+@extends('home.app')
+@section('title', $sertifikat->nama_peserta)
+
+@section('content')
+    <main id="main">
+
+        <!-- Hero Section - Home Page -->
+        <section id="hero" class="hero">
+
+            <img src="{{ asset('assets2') }}/img/hero-bg.jpg" alt="" data-aos="fade-in">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <table class="table table-sm">
+                                    <tbody class="text-center">
+                                        <tr>
+                                            <th scope="row" style="font-size: 32px;">{{ $sertifikat->nama_peserta }}</th>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="2" style="font-size: 12px;">Telah mengikuti pelatihan
+                                                <b>"</b><b>{{ $sertifikat->judul_kegiatan }}</b><b>"</b>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="2">
+                                                <a href="{{ route('sertifikat.download', $sertifikat->id) }}"
+                                                    class="btn btn-danger" style="border-radius: 15px;">Download
+                                                    E-Sertifikat</a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <!-- End Hero Section -->
+
+    </main>
+@endsection
