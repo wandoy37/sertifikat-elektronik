d2afd8794688ac04518a222d334444723340d98b
wandoy37 <riswandi035@gmail.com>
2024-03-03 11:27:42 +0800

    rapikan menu sidebar oprator

--- resources/views/dashboard/layouts/partials/sidebar.blade.php
+++ resources/views/dashboard/layouts/partials/sidebar.blade.php
@@ -39,46 +39,16 @@
                     </a>
                 </li>
                 @if ((Auth::user()->role == 'admin') | (Auth::user()->role == 'oprator'))
-                    <li class="nav-section">
-                        <span class="sidebar-mini-icon">
-                            <i class="fa fa-ellipsis-h"></i>
-                        </span>
-                        <h4 class="text-section">MENU MASTER</h4>
-                    </li>
-                    <li class="nav-item {{ request()->segment(2) == 'pengguna' ? 'active' : '' }}">
+                    {{-- <li class="nav-item {{ request()->segment(2) == 'pengguna' ? 'active' : '' }}">
                         <a href="{{ route('user.index') }}">
                             <i class="fas fa-user"></i>
                             <p>Pengguna</p>
                         </a>
-                    </li>
-                    <li class="nav-item {{ request()->segment(2) == 'peserta' ? 'active' : '' }}">
-                        <a href="{{ route('peserta.index') }}">
-                            <i class="fas fa-users"></i>
-                            <p>Peserta</p>
-                        </a>
-                    </li>
-                    <li class="nav-item {{ request()->segment(2) == 'orang' ? 'active' : '' }}">
-                        <a href="{{ route('orang.index') }}">
-                            <i class="fas fa-user"></i>
-                            <p>Orang</p>
-                        </a>
-                    </li>
-                    <li class="nav-item {{ request()->segment(2) == 'siswa' ? 'active' : '' }}">
-                        <a href="{{ route('siswa.index') }}">
-                            <i class="fas fa-graduation-cap"></i>
-                            <p>Siswa</p>
-                        </a>
-                    </li>
-                    <li class="nav-item {{ request()->segment(2) == 'narasumber' ? 'active' : '' }}">
-                        <a href="{{ route('narasumber.index') }}">
-                            <i class="fas fa-chalkboard-teacher"></i>
-                            <p>Narasumber</p>
-                        </a>
-                    </li>
-                    <li class="nav-item {{ request()->segment(2) == 'penandatangan' ? 'active' : '' }}">
-                        <a href="{{ route('penandatangan.index') }}">
-                            <i class="fas fa-file-signature"></i>
-                            <p>Penandatangan</p>
+                    </li> --}}
+                    <li class="nav-item {{ request()->segment(2) == 'kegiatan' ? 'active' : '' }}">
+                        <a href="{{ route('kegiatan.index') }}">
+                            <i class="fas fa-podcast"></i>
+                            <p>Kegiatan</p>
                         </a>
                     </li>
                     <li class="nav-item {{ request()->segment(2) == 'kategori' ? 'active' : '' }}">
@@ -87,17 +57,47 @@
                             <p>Kategori</p>
                         </a>
                     </li>
-                    <li class="nav-item {{ request()->segment(2) == 'kegiatan' ? 'active' : '' }}">
-                        <a href="{{ route('kegiatan.index') }}">
-                            <i class="fas fa-podcast"></i>
-                            <p>Kegiatan</p>
-                        </a>
-                    </li>
                 @endif
                 <li class="nav-item {{ request()->segment(2) == 'sertifikat' ? 'active' : '' }}">
                     <a href="{{ route('sertifikat.index') }}">
                         <i class="fas fa-certificate"></i>
                         <p>Sertifikat</p>
+                    </a>
+                </li>
+                <li class="nav-section">
+                    <span class="sidebar-mini-icon">
+                        <i class="fa fa-ellipsis-h"></i>
+                    </span>
+                    <h4 class="text-section">DATA MASTER</h4>
+                </li>
+                <li class="nav-item {{ request()->segment(2) == 'peserta' ? 'active' : '' }}">
+                    <a href="{{ route('peserta.index') }}">
+                        <i class="fas fa-users"></i>
+                        <p>Peserta</p>
+                    </a>
+                </li>
+                <li class="nav-item {{ request()->segment(2) == 'orang' ? 'active' : '' }}">
+                    <a href="{{ route('orang.index') }}">
+                        <i class="fas fa-user"></i>
+                        <p>Orang</p>
+                    </a>
+                </li>
+                <li class="nav-item {{ request()->segment(2) == 'siswa' ? 'active' : '' }}">
+                    <a href="{{ route('siswa.index') }}">
+                        <i class="fas fa-graduation-cap"></i>
+                        <p>Siswa</p>
+                    </a>
+                </li>
+                <li class="nav-item {{ request()->segment(2) == 'narasumber' ? 'active' : '' }}">
+                    <a href="{{ route('narasumber.index') }}">
+                        <i class="fas fa-chalkboard-teacher"></i>
+                        <p>Narasumber</p>
+                    </a>
+                </li>
+                <li class="nav-item {{ request()->segment(2) == 'penandatangan' ? 'active' : '' }}">
+                    <a href="{{ route('penandatangan.index') }}">
+                        <i class="fas fa-file-signature"></i>
+                        <p>Penandatangan</p>
                     </a>
                 </li>
 
