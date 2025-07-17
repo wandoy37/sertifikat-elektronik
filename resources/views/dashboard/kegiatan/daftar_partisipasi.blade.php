472f0cb6b13e3d5e9a40a4235c3151c781024f85
wandoy37 <riswandi035@gmail.com>
2025-07-18 05:28:58 +0800

    verify curl

--- resources/views/dashboard/kegiatan/daftar_partisipasi.blade.php
+++ resources/views/dashboard/kegiatan/daftar_partisipasi.blade.php
@@ -29,8 +29,31 @@
                                             @php
                                                 $peserta_id = $sertifikat->peserta_id;
                                                 $url = env('SIMPELTAN_API_DATA_PESERTA') . "/{$sertifikat->peserta_id}";
-                                                $response = file_get_contents($url);
-                                                $data = json_decode($response, true);
+
+                                                $ch = curl_init();
+                                                curl_setopt($ch, CURLOPT_URL, $url);
+                                                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                                                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+                                                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+                                                curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+                                                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+                                                curl_setopt($ch, CURLOPT_USERAGENT, 'Laravel/10.0 (compatible; PHP)');
+
+                                                $response = curl_exec($ch);
+                                                $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                                                $error = curl_error($ch);
+                                                curl_close($ch);
+
+                                                if ($error) {
+                                                    $data = null;
+                                                    // Log error jika perlu
+                                                    \Log::error('cURL Error: ' . $error);
+                                                } elseif ($httpCode !== 200) {
+                                                    $data = null;
+                                                    \Log::error('HTTP Error: ' . $httpCode);
+                                                } else {
+                                                    $data = json_decode($response, true);
+                                                }
                                             @endphp
                                             {{ $data[0]['peserta_nama'] }}
                                         </td>
